06e00dcc806d1774d14886a8fa20199790600469
Charles Scholle <cscholle@breadfinance.com>
2024-06-17 15:25:12 -0400

    01 demo

---
+++ dev-spring-boot/mycoolapp/src/main/java/com/chaz8080/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,15 @@
+package com.chaz8080.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hello World"
+
+    @GetMapping("/")
+    public String getMethodName() {
+        return "Hello World, again!";
+    }
+}
