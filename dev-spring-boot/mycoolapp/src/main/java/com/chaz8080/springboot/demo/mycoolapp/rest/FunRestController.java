7fb2fec910cc282f2213ac6039f40ad50699428d
Charles Scholle <cscholle@breadfinance.com>
2024-06-17 16:07:32 -0400

    actuator

--- dev-spring-boot/mycoolapp/src/main/java/com/chaz8080/springboot/demo/mycoolapp/rest/FunRestController.java
+++ dev-spring-boot/mycoolapp/src/main/java/com/chaz8080/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -9,7 +9,18 @@ public class FunRestController {
     // expose "/" that return "Hello World"
 
     @GetMapping("/")
-    public String getMethodName() {
+    public String sayHello() {
         return "Hello World, again!";
     }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getFortune() {
+        return "Today is your lucky day!";
+    }
+
 }
