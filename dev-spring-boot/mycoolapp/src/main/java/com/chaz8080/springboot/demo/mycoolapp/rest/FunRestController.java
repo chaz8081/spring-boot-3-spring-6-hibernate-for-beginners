9b1194de3ec30352dac30887e783ba7a6dfd69af
Charles Scholle <cscholle@breadfinance.com>
2024-06-20 08:37:03 -0400

    added some props

--- dev-spring-boot/mycoolapp/src/main/java/com/chaz8080/springboot/demo/mycoolapp/rest/FunRestController.java
+++ dev-spring-boot/mycoolapp/src/main/java/com/chaz8080/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,16 +1,25 @@
 package com.chaz8080.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${coach.name}")
+    private String coachName;
 
-    // expose "/" that return "Hello World"
+    @Value("${team.name}")
+    private String teamName;
 
     @GetMapping("/")
     public String sayHello() {
         return "Hello World, again!";
+    }
+
+    @GetMapping("/teamInfo")
+    public String getTeamInfo() {
+        return "Coach Name: " + coachName + ", Team Name: " + teamName;
     }
 
     @GetMapping("/workout")
