e1e3ba1ad982eda78d45100f21fe516053705a65
Charles Scholle <cscholle@breadfinance.com>
2024-06-21 13:39:10 -0400

    setter injection

--- dev-spring-boot/02-spring-boot-spring-core/springcoredemo/src/main/java/com/chaz8080/springcoredemo/SpringcoredemoApplication.java
+++ dev-spring-boot/02-spring-boot-spring-core/springcoredemo/src/main/java/com/chaz8080/springcoredemo/SpringcoredemoApplication.java
@@ -3,10 +3,7 @@ package com.chaz8080.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {
-		"com.chaz8080.springcoredemo",
-		"com.chaz8080.util"
-})
+@SpringBootApplication
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
