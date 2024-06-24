a217174565d210bc2ce786c6d36d2923f897593d
Charles Scholle <cscholle@breadfinance.com>
2024-06-24 16:34:41 -0400

    can create a student

--- dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/chaz8080/cruddemo/CruddemoApplication.java
+++ dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/chaz8080/cruddemo/CruddemoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.chaz8080.cruddemo.dao.StudentDAO;
+import com.chaz8080.cruddemo.entity.Student;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -13,9 +16,19 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] arg) {
+	CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello, World!");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		System.out.println("Creating student...");
+		Student student = new Student("Joe", "Schmo", "joe.schmo@gmail.com");
+
+		System.out.println("Saving student...");
+		studentDAO.save(student);
+
+		System.out.println("Saved student with ID: " + student.getId());
 	}
 }
---
+++ dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/chaz8080/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.chaz8080.cruddemo.dao;
+
+import com.chaz8080.cruddemo.entity.Student;
+
+public interface StudentDAO {
+    void save(Student student);
+}
