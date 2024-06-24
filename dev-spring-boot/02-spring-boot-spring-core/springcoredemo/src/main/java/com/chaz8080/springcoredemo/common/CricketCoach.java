488a1f5baf6a1eb5aac5b398113672f5dee451c2
Charles Scholle <cscholle@breadfinance.com>
2024-06-24 12:37:12 -0400

    bean lifecycle methods

--- dev-spring-boot/02-spring-boot-spring-core/springcoredemo/src/main/java/com/chaz8080/springcoredemo/common/CricketCoach.java
+++ dev-spring-boot/02-spring-boot-spring-core/springcoredemo/src/main/java/com/chaz8080/springcoredemo/common/CricketCoach.java
@@ -2,11 +2,24 @@ package com.chaz8080.springcoredemo.common;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
 public class CricketCoach implements Coach {
 
     public CricketCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff: " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff: " + getClass().getSimpleName());
     }
 
     @Override
