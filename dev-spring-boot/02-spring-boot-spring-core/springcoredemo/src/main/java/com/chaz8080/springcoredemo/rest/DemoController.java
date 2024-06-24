3c3693701183486662aba1d37fa32bd4f73b91f3
Charles Scholle <cscholle@breadfinance.com>
2024-06-24 12:00:28 -0400

    bean scopes

--- dev-spring-boot/02-spring-boot-spring-core/springcoredemo/src/main/java/com/chaz8080/springcoredemo/rest/DemoController.java
+++ dev-spring-boot/02-spring-boot-spring-core/springcoredemo/src/main/java/com/chaz8080/springcoredemo/rest/DemoController.java
@@ -10,15 +10,25 @@ import com.chaz8080.springcoredemo.common.Coach;
 @RestController
 public class DemoController {
     private Coach myCoach;
+    private Coach anotherCoach;
 
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+    public DemoController(
+            @Qualifier("cricketCoach") Coach myCoach,
+            @Qualifier("cricketCoach") Coach anotherCoach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
-        myCoach = theCoach;
+        this.myCoach = myCoach;
+        this.anotherCoach = anotherCoach;
     }
 
     @GetMapping("/dailyWorkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
     }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans: myCoach == anotherCoach, " + (this.myCoach == this.anotherCoach);
+    }
+
 }
