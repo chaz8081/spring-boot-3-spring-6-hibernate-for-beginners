0a04e0023033af13cb579b2bc6862e8bcbba4c3b
Charles Scholle <cscholle@breadfinance.com>
2024-06-24 10:43:46 -0400

    Revert "setter injection"
    This reverts commit e1e3ba1ad982eda78d45100f21fe516053705a65.

--- dev-spring-boot/02-spring-boot-spring-core/springcoredemo/src/main/java/com/chaz8080/springcoredemo/SpringcoredemoApplication.java
+++ dev-spring-boot/02-spring-boot-spring-core/springcoredemo/src/main/java/com/chaz8080/springcoredemo/SpringcoredemoApplication.java
@@ -3,7 +3,10 @@ package com.chaz8080.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"com.chaz8080.springcoredemo",
+		"com.chaz8080.util"
+})
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
